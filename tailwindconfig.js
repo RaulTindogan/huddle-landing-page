636165e588528fb39a92eb6cf88f54bc65513abc
RaulTindogan <tindoganraul@gmail.com>
2024-04-26 22:47:18 +0800

    initial commit

---
+++ tailwindconfig.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+            "Violet": "hsl(257, 40%, 49%)",
+            "Soft-Magenta": "hsl(300, 69%, 71%)"
+        }, 
+        fontFamily: {
+           "Open-Sans": ["Open Sans", "sans-serif"],
+           "Poppins": ["Poppins", "sans-serif"]
+        }
+      }
+    }
+  }
